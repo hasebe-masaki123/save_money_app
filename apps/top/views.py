2b9a1a5f3f49009f1b30e9c587f1445b236cfbb3
kasuyasara <tky2402056@stu.o-hara.ac.jp>
2025-11-20 12:02:17 +0900

    top,通知

--- apps/top/views.py
+++ apps/top/views.py
@@ -1,14 +1,14 @@
 from flask import request,render_template,url_for,Blueprint
-from flask_login import current_user
-from app import db,os
-import app
-from apps.goal.models import Goals
-from apps.income.models import Income
-from apps.expense.models import Expense
-from apps.saving.models import Saving
-from sqlalchemy import func
+# from flask_login import current_user
+# from app import db,os
+# import app
+# from apps.goal.models import Goal
+# from apps.income.models import Income
+# from apps.expense.models import Expense
+# from apps.saving.models import Saving
+# from sqlalchemy import func
 import matplotlib.pyplot as plt
-from datetime import datetime
+# from datetime import datetime
 
 top = Blueprint(
     "top",
@@ -18,7 +18,7 @@ top = Blueprint(
 )
 
 @top.route("/top", methods=["GET", "POST"])
-def top():
+def top_page():
     balance,total_income_amount,total_saving_amount,auto_saving = week_balance()
 
     return render_template("top/top.html",balance=balance)
@@ -26,22 +26,34 @@ def top():
 #今週の残金を表示
 def week_balance():
     #目標金額、現在の達成額、目標期日、収入、支出、貯金をＤＢから取得
-    total_income_amount = db.session.query(func.sum(Income.amount)).filter(Income.user_id == current_user.id).scalar() or 0
-    total_expense_amount = db.session.query(func.sum(Expense.amount)).filter(Expense.user_id == current_user.id).scalar() or 0
-    total_saving_amount = db.session.query(func.sum(Saving.amount)).filter(Saving.user_id == current_user.id).scalar() or 0
+    # total_income_amount = db.session.query(func.sum(Income.amount)).filter(Income.user_id == current_user.id).scalar() or 0
+    # total_expense_amount = db.session.query(func.sum(Expense.amount)).filter(Expense.user_id == current_user.id).scalar() or 0
+    # total_saving_amount = db.session.query(func.sum(Saving.amount)).filter(Saving.user_id == current_user.id).scalar() or 0
 
-    goal = db.session.query(Goals).filter(Goals.user_id == current_user.id).first()
-    if goal:
-        target_amount = goal.TARGET_AMOUNT or 0
-        current_amount = goal.CURRENT_AMOUNT or 0
-        #今日の日付と期限日を引いて、残りの日数を求める。
-        deadline_days = (goal.DEADLINE_AT - datetime.today()).days if goal.DEADLINE_AT else 1
-        deadline_days = max(deadline_days, 1)  # 0除算防止
+    total_income_amount =100000
+    total_expense_amount = 50000
+    total_saving_amount = 10000
 
-        auto_saving = (target_amount - current_amount) / deadline_days
+    # goal = db.session.query(Goal).filter(Goal.user_id == current_user.id).first()
+    # if goal:
+    #     target_amount = goal.TARGET_AMOUNT or 0
+    #     current_amount = goal.CURRENT_AMOUNT or 0
+    #     #今日の日付と期限日を引いて、残りの日数を求める。
+    #     deadline_days = (goal.DEADLINE_AT - datetime.today()).days if goal.DEADLINE_AT else 1
+    #     deadline_days = max(deadline_days, 1)  # 0除算防止
+
+    target_amount = 200000
+    current_amount  = 100000
+    deadline_days = 60
+
+    auto_saving = (target_amount - current_amount) / deadline_days or 0
+    # else:
+    #     auto_saving = 0
+    # balance = total_income_amount - total_expense_amount - total_saving_amount - auto_saving
+    if auto_saving==0:
+        balance = total_income_amount - total_expense_amount - total_saving_amount
     else:
-        auto_saving = 0
-    balance = total_income_amount - total_expense_amount - total_saving_amount - auto_saving
+        balance = total_income_amount - total_expense_amount - total_saving_amount - auto_saving
 
     return balance,total_income_amount,total_saving_amount,auto_saving
 
